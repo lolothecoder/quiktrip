46026f45e5438f58f4e97cf55b01e98bcb57d661
Lolo <loicfinette@gmail.com>
2023-03-06 16:44:35 +0100

    Improved phone display

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -28,3 +28,29 @@ function linkAction(){
     navMenu.classList.remove('show-menu');
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
+
+var slideIndex = 0;
+showSlides();
+var slidesTimeout;
+
+function showSlides() {
+  console.log(slideIndex);
+  let i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  if (slideIndex > slides.length-1) {slideIndex = 0;}
+  if (slideIndex < 0) {slideIndex = slides.length-1;}
+  slideIndex++;
+  slides[slideIndex-1].style.display = "block";
+  slidesTimeout = setTimeout(showSlides, 10000); // Change image every 10 seconds
+}
+
+function plusSlides(n) {
+  clearTimeout(slidesTimeout);
+  if (n < 0) {slideIndex -= 2;}
+  showSlides();
+  //slideIndex += n;
+  //console.log(slideIndex);
+}
